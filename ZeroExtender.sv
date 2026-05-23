9431573451b6515ad52c0e034c361b2e85b21384
alin-121 <andrewlin990@gmail.com>
2026-05-22 23:22:41 -0700

    Mass delete

--- ZeroExtender.sv
+++
@@ -1,8 +0,0 @@
-module ZeroExtender #(parameter INPUT = 64, parameter EXTEND_TO = 64) (in, out);
-
-    input  logic [INPUT-1:0] in;
-    output logic [EXTEND_TO-1:0] out;
-
-    assign out = {{(EXTEND_TO - INPUT){1'b0}}, in};
-
-endmodule
